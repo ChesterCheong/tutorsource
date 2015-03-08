73fbcb389d600fc75ff41429e9d51f26ecaefa46
ChesterCheong <wtcheong.2012@sis.smu.edu.sg>
2015-03-08 16:47:31 +0000

    almost done

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 //
 //
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 9001));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
