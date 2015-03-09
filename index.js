6a4dba9cf9c9849140da85d17a5f3d549db231cf
ChesterCheong <wtcheong.2012@sis.smu.edu.sg>
2015-03-09 22:57:03 +0800

    Update index.js

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ app.get('/payment/initiate/:planId', function (req, res) {
 });
 
 //execute payment for plan. this endpoint is called when the user has paided via PayPal
-app.get('//payment/execute/', function (req, res) {
+app.get('/payment/execute/', function (req, res) {
     // TODO: using information in the session, and agreement ID, store the information in Firebase
     //checks if there is a token
     if(req.query.token){
